40d0724f80a3f4083b39878df1aeee83818822a6
prajula255 <prajulasouparnika3@gmail.com>
2024-11-08 10:26:29 +0530

    registration updated

--- src/components/pages/reg/reg.jsx
+++ src/components/pages/reg/reg.jsx
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import TextInput from "../../text-input";
 import Button from "../../Button";
 
 export default function RegistrationPage() {
+    const [Username, setUsername] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setpassword] = useState("")
+
+    function handleRegister() {
+        alert(`Username:${Username},Email:${email},Password:${password}`)
+    }
     return (
         <React.Fragment>
             <div className="d-flex justify-content-center align-items-center" style={{ height: "100svh", backgroundColor: "#eee" }}>
-                <form className="bg-white p-3 w-25 rounded-3 text-center">
+                <div className="bg-white p-3 w-25 rounded-3 text-center">
                     <p className="fs-4 fw-semibold">Registration</p>
 
-                    <TextInput type={"text"} placeholder={"Enter username"} />
-                    <TextInput type={"email"} placeholder={"Enter email"} />
-                    <TextInput type={"password"} placeholder={"Enter password"} />
-                    <Button label={"Register"} />
+                    <TextInput type={"text"} placeholder={"Enter username"} onChange={(e)=>setUsername(e.target.value)} />
+                    <TextInput type={"email"} placeholder={"Enter email"}onChange={(e) => setEmail(e.target.value)} />
+                    <TextInput type={"password"} placeholder={"Enter password"} onChange={(e)=>setpassword(e.target.value)} />
+                    <Button label={"Register"} onClick={handleRegister} />
 
 
-                </form></div>
+                </div></div>
 
         </React.Fragment>
     )
